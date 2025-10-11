af0f7481e3e6047d49284d413977f336b9ca9e33
nOester <nils.oesterling@swisstopo.ch>
2025-10-11 10:11:06 +0200

    remove unnessary code

--- api/api_app/helpers.py
+++ api/api_app/helpers.py
@@ -5,54 +5,50 @@ import json
 import re
 
 def fetch_and_save_wms_xml(
-    save_dir="tmp",
-    xml_filename="service_response.xml",
-    json_filename="service_response.json"
+    save_dir="tmp"
 ):
     """
-    Ruft eine fest hinterlegte WMS-URL ab und speichert die Response als XML-Datei.
+    Call a hard coded WMS- or WMTS-URL and save response as XML and convert ans save it as JSON
     """
 
-    # Deine WMS-URL (hartkodiert)
+    #  WMS- and WMTS-URL (hard coded)
     wms_url = "https://wms.geo.admin.ch/?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetCapabilities"
     wmts_url = "https://wmts.geo.admin.ch/EPSG/2056/1.0.0/WMTSCapabilities.xml"
 
     urls = [wms_url, wmts_url]
 
-    # Speicherordner anlegen
+    # Define storage directory
     os.makedirs(save_dir, exist_ok=True)
-    # xml_path = os.path.join(save_dir, xml_filename)
-    # json_path = os.path.join(save_dir, json_filename)
 
     try:
         for url in urls:
             print(url)
 
+            # Filter Characters after "https://" and "." in service-URL 
             match = re.search(r"^https://([^\.]+)\.", url)
             if match:
-                typ = match.group(1)  # speichert "wms" bzw. "wmts"
+                typ = match.group(1)  # saves "wms" or "wmts"
                 print(typ)
 
             xml_path = os.path.join(save_dir, f'{typ}.xml')
             json_path = os.path.join(save_dir,  f'{typ}.json')
 
-            # Request an den WMS-Server
-            # response = requests.get(wms_url, timeout=20)
+            # Request WMS-Server
             response = requests.get(url, timeout=20)
             response.raise_for_status()
 
-            # Response als XML speichern
+            # Save response as XML 
             with open(xml_path, "w", encoding="utf-8") as f:
                 f.write(response.text)
 
-            # XML in Python-Dict konvertieren
+            # Convert XML in Python-Dict
             data_dict = xmltodict.parse(response.text)
 
-            # JSON speichern
+            # Save speichern
             with open(json_path, "w", encoding="utf-8") as f:
                 json.dump(data_dict, f, indent=2)    
 
         return xml_path, json_path, data_dict
 
     except requests.exceptions.RequestException as e:
-        raise RuntimeError(f"Fehler beim Abrufen der WMS-URL: {e}")
+        raise RuntimeError(f"Error when calling WMS- or WMTS-URL: {e}")
